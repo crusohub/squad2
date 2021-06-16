91f01aab65350b10ee99a71f77164ab9de65842f
antoniotrindade2304 <80802366+antoniotrindade2304@users.noreply.github.com>
2021-06-16 02:29:21 -0300

    front-end ProjectAssociation

---
+++ src/views/examples/ProjectAssociation.js
@@ -0,0 +1,143 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { useState, useEffect } from "react";
+
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  Container,
+  Row,
+  Button,
+  UncontrolledTooltip,
+} from "reactstrap";
+// core components
+import HeaderProject from "components/Headers/HeaderProject";
+
+
+const ProjectAssociation = () => {
+
+  const [userValue, setUserValue] = useState('');
+  const [projectValue, setProjectValue] = useState('');
+  const [erros, setErros] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [listUsers, setListUsers] = useState([]);
+  const [listProjects, setListProjects] = useState([]);
+
+  const validarCampos = () => {
+    if (userValue != '' && projectValue != '') {
+      setErros(false);
+      return true;
+    } else {
+      setErros(true);
+      if (userValue == '' && projectValue != '') {
+        setErrorMessage("Select a user");
+      } else {
+        if (projectValue == '' && userValue != '') {
+          setErrorMessage("Select a project");
+        } else {
+          setErrorMessage("Select a user and project");
+        };
+      };
+      return false;
+    };
+  };
+
+  const preencherSelectUsers = () => {
+    let url = 'https://60bfbc0397295a0017c43b7a.mockapi.io/usuario';
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setListUsers([...data]);
+      });
+  };
+
+  const preencherSelectProjects = () => {
+    let url = 'https://60bfbc0397295a0017c43b7a.mockapi.io/projeto';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setListProjects([...data]);
+      });
+  };
+
+  const efetuarLogin = () => {
+    if (validarCampos() == true) {
+      console.log("passou");
+    }
+  };
+
+  useEffect(() => {
+    preencherSelectUsers();
+    preencherSelectProjects();
+
+  }, []);
+
+  return (
+    <>
+      <HeaderProject />
+      <Container className="mt--7" fluid>
+        <Row>
+          <div className="col text-center">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <h3 className="mb-0">User Project Association</h3>
+              </CardHeader>
+
+              {/* Select de projetos */}
+              <div className="col">
+                <span className="display-5 text-center">Select a project:</span>
+                <select onChange={(e) => setProjectValue(e.target.value)} className="form-control mx-auto mb-3" tabindex="2" style={{ maxWidth: 300, cursor: "pointer,m" }}>
+                  <option>Select a project</option>
+                  {listProjects.map((data) => (
+                    <option value={data.id}>{data.projectname}</option>
+                  ))}
+                </select>
+
+                {/* Select de usuários */}
+                <span className="display-5 text-center">Select a user:</span>
+                <select onChange={(e) => setUserValue(e.target.value)} className="form-control mx-auto" tabindex="2" style={{ maxWidth: 300 }}>
+                  <option>Select a user</option>
+                  {listUsers.map((data) => (
+                    <option value={data.id}>{data.username}</option>
+                  ))}
+                </select>
+
+                {/* Botão de associar projeto */}   
+                <Button className="my-4" color="primary" type="button" onClick={efetuarLogin}>
+                  Associate project
+                </Button>
+                  
+                {/* Div para mostragem de erros */}
+                {erros &&
+                  <div className="col">
+                    <span style={{ color: "#ff0000", fontWeight: "bold" }}>{errorMessage}</span>
+                  </div>
+                }
+              </div>
+            </Card>
+          </div>
+        </Row>
+
+      </Container>
+    </>
+  );
+};
+
+export default ProjectAssociation;
