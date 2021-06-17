b89aef4a47c4dfcc0422dc245be7b0431fd6de91
DuanCamargo <duan1505@gmail.com>
2021-06-17 14:29:36 -0300

    fix

--- src/views/examples/ProfileSettings.js
+++ src/views/examples/ProfileSettings.js
@@ -1,7 +1,7 @@
 
 import React, { useContext, useState, useEffect } from "react";
-import UsuarioDataService from"services/UsuarioDataService"
-import {UsuarioLogadoContext} from "context/UsuarioLogadoContext"
+import UsuarioDataService from "services/UsuarioDataService"
+import { UsuarioLogadoContext } from "context/UsuarioLogadoContext"
 import { Link } from "react-router-dom";
 
 // reactstrap components
@@ -20,59 +20,42 @@ import {
 } from "reactstrap";
 
 const Settings = (props) => {
-    // const [currentPassword, setCurrentPassword] = useState("")
-    // const [newPassword, setNewPassword] = useState("")
-    const [user, setUser] =useContext(UsuarioLogadoContext)
+    const [user, setUser] = useContext(UsuarioLogadoContext)
 
-    const getCurrentPassword = (id) => {
-        UsuarioDataService.get(id)
-        .then(response => {
-            setUser(response.data)
-        })
-        .catch(e => {
-            console.log(e);
-        })
-    }
-
-    // const handleChange = (e) => {
-    //     const { name, value } = e.target;
-    //     setUser({ ...user, [name]: value })
-    // }
-    
     const updatePassword = (id, psw) => {
         UsuarioDataService.update(id, psw)
-        .then(response => {
-            setUser(response.data)
-        })
-        .catch(e => {
-            console.log(e);
-        })
+            .then(response => {
+                setUser(response.data)
+            })
+            .catch(e => {
+                console.log(e);
+            })
+        alert("Password Updated!!!")
     }
 
     useEffect(() => {
-        getCurrentPassword(75);
-    },[]);
+
+    }, []);
 
     const resetPassword = () => {
-        const contexCurrentPassword = user.password
         const inputCurrentPassword = (document.querySelector("#currentPassword").value)
         const newPassword = (document.querySelector("#newPasswod").value)
         const reNewPasswod = (document.querySelector("#reNewPasswod").value)
 
-        if(inputCurrentPassword == ""){
+        if (inputCurrentPassword === "") {
             alert("Insira o password atual!")
             return false
-        }else if(inputCurrentPassword !== contexCurrentPassword){
+        } else if (inputCurrentPassword !== user.password) {
             alert("O password inserido não é o password atual.\nPor favor insira o password atual!")
             return false
         }
 
-        if(newPassword == ""){
+        if (newPassword === "") {
             alert("Insira o seu novo password!")
             return false
         }
 
-        if(newPassword !== reNewPasswod){
+        if (newPassword !== reNewPasswod) {
             alert("Os password tem que ser iguais em:\nNew Password e Re-Entre New Password")
             return false
         }
@@ -80,7 +63,7 @@ const Settings = (props) => {
         updatePassword(user.id, user)
     }
 
-    return(
+    return (
         <Col lg="5" md="7">
             <Card className="bg-secondary shadow border-0">
                 <CardBody className="px-lg-5 py-lg-5">
@@ -89,13 +72,14 @@ const Settings = (props) => {
                             <InputGroup className="input-group-alternative">
                                 <InputGroupAddon addonType="prepend">
                                     <InputGroupText>
-                                    <i className="ni ni-lock-circle-open" />
+                                        <i className="ni ni-lock-circle-open" />
                                     </InputGroupText>
                                 </InputGroupAddon>
                                 <Input
                                     id="currentPassword"
                                     placeholder="Current Password"
                                     type="password"
+                                // value={user.password}
                                 />
                             </InputGroup>
                         </FormGroup>
@@ -132,11 +116,11 @@ const Settings = (props) => {
                         <div className="text-center">
                             <Button className="my-4" color="danger" type="button">
                                 <Link to={"/admin/user-profile"}
-                                color = "white">
+                                    color="white">
                                     Cancel
                                 </Link>
                             </Button>
-                            <Button className="my-4" color="primary" type="button" onClick = {() => resetPassword()} >
+                            <Button className="my-4" color="primary" type="button" onClick={() => resetPassword()} >
                                 Reset Password
                             </Button>
                         </div>
@@ -150,7 +134,7 @@ const Settings = (props) => {
                         href="#pablo"
                         onClick={(e) => e.preventDefault()}
                     >
-                    <small>Forgot password?</small>
+                        <small>Forgot password?</small>
                     </a>
                 </Col>
                 <Col className="text-right" xs="6">
