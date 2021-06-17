69a838065a53d694624e632059da180dc4e9b3e1
DuanCamargo <duan1505@gmail.com>
2021-06-17 13:46:00 -0300

    pre_aula

--- src/views/examples/ProfileSettings.js
+++ src/views/examples/ProfileSettings.js
@@ -1,5 +1,8 @@
 
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import UsuarioDataService from"services/UsuarioDataService"
+import {UsuarioLogadoContext} from "context/UsuarioLogadoContext"
+import { Link } from "react-router-dom";
 
 // reactstrap components
 import {
@@ -16,10 +19,67 @@ import {
     Col,
 } from "reactstrap";
 
-import { Link } from "react-router-dom";
-
 const Settings = (props) => {
+    // const [currentPassword, setCurrentPassword] = useState("")
+    // const [newPassword, setNewPassword] = useState("")
+    const [user, setUser] =useContext(UsuarioLogadoContext)
+
+    const getCurrentPassword = (id) => {
+        UsuarioDataService.get(id)
+        .then(response => {
+            setUser(response.data)
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    }
+
+    // const handleChange = (e) => {
+    //     const { name, value } = e.target;
+    //     setUser({ ...user, [name]: value })
+    // }
     
+    const updatePassword = (id, psw) => {
+        UsuarioDataService.update(id, psw)
+        .then(response => {
+            setUser(response.data)
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    }
+
+    useEffect(() => {
+        getCurrentPassword(75);
+    },[]);
+
+    const resetPassword = () => {
+        const contexCurrentPassword = user.password
+        const inputCurrentPassword = (document.querySelector("#currentPassword").value)
+        const newPassword = (document.querySelector("#newPasswod").value)
+        const reNewPasswod = (document.querySelector("#reNewPasswod").value)
+
+        if(inputCurrentPassword == ""){
+            alert("Insira o password atual!")
+            return false
+        }else if(inputCurrentPassword !== contexCurrentPassword){
+            alert("O password inserido não é o password atual.\nPor favor insira o password atual!")
+            return false
+        }
+
+        if(newPassword == ""){
+            alert("Insira o seu novo password!")
+            return false
+        }
+
+        if(newPassword !== reNewPasswod){
+            alert("Os password tem que ser iguais em:\nNew Password e Re-Entre New Password")
+            return false
+        }
+
+        updatePassword(user.id, user)
+    }
+
     return(
         <Col lg="5" md="7">
             <Card className="bg-secondary shadow border-0">
@@ -33,6 +93,7 @@ const Settings = (props) => {
                                     </InputGroupText>
                                 </InputGroupAddon>
                                 <Input
+                                    id="currentPassword"
                                     placeholder="Current Password"
                                     type="password"
                                 />
@@ -46,6 +107,7 @@ const Settings = (props) => {
                                     </InputGroupText>
                                 </InputGroupAddon>
                                 <Input
+                                    id="newPasswod"
                                     placeholder="New Password"
                                     type="password"
                                     autoComplete="new-password"
@@ -60,6 +122,7 @@ const Settings = (props) => {
                                     </InputGroupText>
                                 </InputGroupAddon>
                                 <Input
+                                    id="reNewPasswod"
                                     placeholder="Re-enter the New Password"
                                     type="password"
                                     autoComplete="new-password"
@@ -73,7 +136,7 @@ const Settings = (props) => {
                                     Cancel
                                 </Link>
                             </Button>
-                            <Button className="my-4" color="primary" type="button">
+                            <Button className="my-4" color="primary" type="button" onClick = {() => resetPassword()} >
                                 Reset Password
                             </Button>
                         </div>
