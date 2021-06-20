c39aafdd0b1730ddd2dd16535977274976f5c307
Julyellen Santos <julyellensantos1818@gmail.com>
2021-06-20 18:11:42 -0300

    Card Total Users

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -15,12 +15,30 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
+import React, { useState, useEffect }from "react";
 
 // reactstrap components
 import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+import UsuarioDataService from "services/UsuarioDataService";
 
 const Header = () => {
+
+  const [users, setUsers] = useState([])
+
+  const getUsers = () => {
+    UsuarioDataService.getAll()
+    .then(response => {
+      setUsers(response.data)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+  }
+
+  useEffect(() => {
+    getUsers()
+  })
+
   return (
     <>
       <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
@@ -37,10 +55,10 @@ const Header = () => {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          Traffic
+                          Total Users
                         </CardTitle>
                         <span className="h2 font-weight-bold mb-0">
-                          350,897
+                          {users.length}
                         </span>
                       </div>
                       <Col className="col-auto">
@@ -49,12 +67,12 @@ const Header = () => {
                         </div>
                       </Col>
                     </Row>
-                    <p className="mt-3 mb-0 text-muted text-sm">
+                    {/*<p className="mt-3 mb-0 text-muted text-sm">
                       <span className="text-success mr-2">
                         <i className="fa fa-arrow-up" /> 3.48%
                       </span>{" "}
                       <span className="text-nowrap">Since last month</span>
-                    </p>
+                      </p>*/}
                   </CardBody>
                 </Card>
               </Col>
@@ -67,7 +85,7 @@ const Header = () => {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          New users
+                          Total projects
                         </CardTitle>
                         <span className="h2 font-weight-bold mb-0">2,356</span>
                       </div>
@@ -77,12 +95,12 @@ const Header = () => {
                         </div>
                       </Col>
                     </Row>
-                    <p className="mt-3 mb-0 text-muted text-sm">
+                    {/*<p className="mt-3 mb-0 text-muted text-sm">
                       <span className="text-danger mr-2">
                         <i className="fas fa-arrow-down" /> 3.48%
                       </span>{" "}
                       <span className="text-nowrap">Since last week</span>
-                    </p>
+                    </p>*/}
                   </CardBody>
                 </Card>
               </Col>
@@ -95,7 +113,7 @@ const Header = () => {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          Sales
+                          Total connections
                         </CardTitle>
                         <span className="h2 font-weight-bold mb-0">924</span>
                       </div>
@@ -105,12 +123,12 @@ const Header = () => {
                         </div>
                       </Col>
                     </Row>
-                    <p className="mt-3 mb-0 text-muted text-sm">
+                    {/*<p className="mt-3 mb-0 text-muted text-sm">
                       <span className="text-warning mr-2">
                         <i className="fas fa-arrow-down" /> 1.10%
                       </span>{" "}
                       <span className="text-nowrap">Since yesterday</span>
-                    </p>
+                    </p>*/}
                   </CardBody>
                 </Card>
               </Col>
@@ -123,7 +141,7 @@ const Header = () => {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          Performance
+                          Average people per project
                         </CardTitle>
                         <span className="h2 font-weight-bold mb-0">49,65%</span>
                       </div>
@@ -133,12 +151,12 @@ const Header = () => {
                         </div>
                       </Col>
                     </Row>
-                    <p className="mt-3 mb-0 text-muted text-sm">
+                    {/*<p className="mt-3 mb-0 text-muted text-sm">
                       <span className="text-success mr-2">
                         <i className="fas fa-arrow-up" /> 12%
                       </span>{" "}
                       <span className="text-nowrap">Since last month</span>
-                    </p>
+                    </p>*/}
                   </CardBody>
                 </Card>
               </Col>
@@ -149,5 +167,4 @@ const Header = () => {
     </>
   );
 };
-
 export default Header;
