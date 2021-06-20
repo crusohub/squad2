b66f91662a06e083a52a3af2385af8fd3fcdf03c
Andrei <andrei09ka6@gmail.com>
2021-06-20 11:20:21 -0300

    Header Generico

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -23,6 +23,9 @@ import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
 const Header = () => {
   return (
     <>
+   
+ 
+
       <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
         <Container fluid>
           <div className="header-body">
@@ -99,6 +102,7 @@ const Header = () => {
                         </CardTitle>
                         <span className="h2 font-weight-bold mb-0">924</span>
                       </div>
+                      
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
                           <i className="fas fa-users" />
