04541087c8ae503b8c81d2b69b7e43987887a268
Marcelo Lino <marcelo.lino128@hotmail.com>
2021-06-21 18:04:20 -0300

    ConnectionsAssociation parcialmente pronta

--- src/views/examples/ConnectionsAssociation.js
+++ src/views/examples/ConnectionsAssociation.js
@@ -1,25 +1,10 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import AssociacaoDataService from "services/AssociacaoDataService";
 import UsuarioDataService from "services/UsuarioDataService";
 import ConexaoDataService from "services/ConexaoDataService";
+import { UsuarioLogadoContext } from "../../context/UsuarioLogadoContext";
 // reactstrap components
+
 import {
     Badge,
     Card,
@@ -54,6 +39,24 @@ const ConnectionsAssociation = () => {
     const [searchUsername, setSearchUsername] = useState("");
     const [searchProjectname, setSearchProjectname] = useState("");
     const [users, setUsers] = useState([]);
+    const [usuarioLogado, setUsuarioLogado] = useContext(UsuarioLogadoContext)
+    const [conexao, setConexao] = useState();
+
+    
+    const handleInputUsername = (e) => {
+    const { value } = e.target;
+    let id = value.split(":")[0];
+    let username = value.split(":")[1];
+    if(username == undefined){
+      username = "Select a user";
+    }
+    setConexao({
+      ...conexao,
+        "userid": id,
+        "username": username 
+    });
+
+  }
 
     const retrieveAssociation = () => {
         AssociacaoDataService.getAll()
@@ -106,15 +109,20 @@ const ConnectionsAssociation = () => {
         }
     };
 
-    /* async function zapName(id) {
-        var name = "";
-        await UsuarioDataService.findById(id).then((response) => {
-            const userZap = response.data[0];
-            name = `${userZap.firstname} ${userZap.lastname}`;
-        });
-        console.log(name);
-        return name;
-    } */
+    const connectOnClick = (e) => {
+        const data = {
+        "userid": usuarioLogado.id,
+        "username": usuarioLogado.username,
+        "useridconnected": conexao.userid,
+        "usernameconnected": conexao.username
+        }
+        console.log(conexao)
+     ConexaoDataService.create(data).then(
+        response=>console.log(response.data)
+    )
+    };
+
+    
 
     useEffect(() => {
         retrieveAssociation();
@@ -135,24 +143,27 @@ const ConnectionsAssociation = () => {
                             </CardHeader>
                             <CardBody>
                                 <Row>
-                                    <Col>
-                                        <label
-                                            className="form-control-label"
-                                            htmlFor="currentPassword"
-                                        >
-                                            User name
-                                        </label>
-                                        <Input
-                                            placeholder="Search by user"
-                                            className="form-control-alternative"
-                                            onChange={(e) =>
-                                                setSearchUsername(
-                                                    e.target.value
-                                                )
-                                            }
-                                            //value={}
-                                        />
-                                    </Col>
+                                     <Col>
+
+                    <label
+                      className="form-control-label"
+                      htmlFor="currentPassword"
+                    >
+                      Select a user
+                    </label>
+                    <Input
+                      name = "username"
+                      type="select"
+                      placeholder="Search by username"
+                      onChange={handleInputUsername}
+                      className="form-control-alternative"
+                    >
+                      <option>Select a user</option>
+                      {users.map((data) => (
+                        <option id = {data.userid} value={data.id + ":"+ data.username}>{data.username}</option>
+                      ))}
+                    </Input>
+                  </Col>
                                     
                                 </Row>
                                 <Row>
@@ -161,9 +172,10 @@ const ConnectionsAssociation = () => {
                                             className="my-4"
                                             color="primary"
                                             type="submit"
-                                            onClick={searchOnClick}
+                                            onClick={connectOnClick}
+                                            
                                         >
-                                            Search
+                                            Connect
                                         </Button>
                                     </Col>
                                 </Row>
