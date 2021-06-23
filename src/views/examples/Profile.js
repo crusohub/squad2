ebe6b8ced7fd167cb26b7b0f8a23ff0666441b52
Pedro da Conceicao <pedrocardo007@hotmail.com>
2021-06-23 12:36:40 -0300

    retirando coisas desnecessarias do perfil

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -117,25 +117,7 @@ const Profile = (props) => {
               <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
               </CardHeader>
               <CardBody className="pt-0 pt-md-4">
-                <Row>
-                  <div className="col">
-                    <div className="card-profile-stats d-flex justify-content-center mt-md-5">
-                      <div>
-                        <span className="heading">22</span>
-                        <span className="description">Friends</span>
-                      </div>
-                      <div>
-                        <span className="heading">10</span>
-                        <span className="description">Photos</span>
-                      </div>
-                      <div>
-                        <span className="heading">89</span>
-                        <span className="description">Comments</span>
-                      </div>
-                    </div>
-                  </div>
-                </Row>
-                <div className="text-center">
+                <div className="text-center mt-5">
                   <h3>
                     {usuarioLogado.username}
                     <span className="font-weight-light">, 27</span>
